39bef735ce79714915ca404b1202282fac480ec6
tanyjnaaman <tannaaman@hotmail.com>
2022-08-31 23:33:57 +0800

    Update filemanager

--- Team05/Code05/src/spa/src/common/FileIOManager.cpp
+++ Team05/Code05/src/spa/src/common/FileIOManager.cpp
@@ -7,10 +7,6 @@ using namespace std;
 
 void FileIOManager::readLinesFromFile(ifstream& stream, const string& filename) {
 
-    try {
-        stream.open(filename);
-    }
-    catch (exception e) {
-        exit(0);
-    }
+	stream.open(filename);
+
 };
--- Team05/Code05/src/spa/src/common/FileIOManager.h
+++ Team05/Code05/src/spa/src/common/FileIOManager.h
@@ -9,7 +9,8 @@ class FileIOManager {
 
 public:
 	/*
-		Reads in a file stream by reference.
+		Reads in a file stream by reference. Note that any errors in reading 
+		will be thrown and propagated to the caller.
 	*/
 	void readLinesFromFile(ifstream& stream, const string& filename);
 };
