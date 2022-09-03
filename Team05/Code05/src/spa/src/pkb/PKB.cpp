6ab376b2420b6c16481f29291834eccbe71ed6a5
nicholas-gcc <nicholasgcc@gmail.com>
2022-09-03 12:07:46 +0800

    Add implementation of PKB dispatching query to table managers

--- Team05/Code05/src/spa/src/pkb/PKB.cpp
+++ Team05/Code05/src/spa/src/pkb/PKB.cpp
@@ -1,7 +1,10 @@
 #include <vector>
+#include <map>
 #include "PKB.h"
 #include <sp/dataclasses/design_objects/Entity.h>
 #include <qps/query/clause/Clause.h>
+
+using namespace std;
 
 ProgramKnowledgeBase::ProgramKnowledgeBase() {
 
@@ -19,7 +22,48 @@ std::vector<Entity> ProgramKnowledgeBase::retrieveAllKnowledge() {
 }
 
 Entity ProgramKnowledgeBase::retrieveKnowledge(Clause queryClause) {
-    return Entity();
+    enum clauseType {
+        FollowsClause,
+        FollowsTClause,
+        ModifiesPClause,
+        ModifiesSClause,
+        ParentClause,
+        ParentTClause,
+        PatternClause,
+        UsesPClause,
+        UsesSClause
+    };
+
+    map<string, clauseType> s_mapClauseTypes;
+
+    string queryClauseType = typeid(queryClause).name();
+
+    // TODO: Optimise this switch case
+    switch(s_mapClauseTypes[queryClauseType]) {
+        case FollowsClause:
+            // stub argument into filter
+            tableManagers[1]->filter(PkbQuery());
+            break;
+        case FollowsTClause:
+            tableManagers[2]->filter(PkbQuery());
+            break;
+        case ModifiesPClause:
+        case ModifiesSClause:
+            tableManagers[3]->filter(PkbQuery());
+            break;
+        case ParentClause:
+            tableManagers[4]->filter(PkbQuery());
+            break;
+        case ParentTClause:
+            tableManagers[5]->filter(PkbQuery());
+            break;
+        case PatternClause:
+            tableManagers[6]->filter(PkbQuery());
+        case UsesPClause:
+        case UsesSClause:
+            tableManagers[7]->filter(PkbQuery());
+            break;
+    };
 }
 
 // TODO Refactor to dataclass coming from QPS
