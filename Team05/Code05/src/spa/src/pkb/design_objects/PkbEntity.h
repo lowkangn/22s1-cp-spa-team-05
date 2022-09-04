0e7e8406fd0477302ee3223e98aad91ec3a29a11
lowkangn <kangngee62@gmail.com>
2022-09-04 14:59:54 +0800

    Incorporate PR comments

--- Team05/Code05/src/spa/src/pkb/design_objects/PkbEntity.h
+++ Team05/Code05/src/spa/src/pkb/design_objects/PkbEntity.h
@@ -2,7 +2,6 @@
 
 #include <string>
 #include <vector>
-#include <sp/dataclasses/tokens/Token.h>
 
 using namespace std;
 
--- Team05/Code05/src/spa/src/pkb/design_objects/PkbRelationship.h
+++ Team05/Code05/src/spa/src/pkb/design_objects/PkbRelationship.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <vector>
-#include <sp/dataclasses/tokens/Token.h>
 
 using namespace std;
 
