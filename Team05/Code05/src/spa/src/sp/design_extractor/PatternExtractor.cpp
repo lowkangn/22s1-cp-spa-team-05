3a7950c9956eb60801f6043510b99c545c397a26
cpwill01 <williamming778@gmail.com>
2022-09-15 23:40:45 +0800

    Add missing includes to PatternExtractor

--- Team05/Code05/src/spa/src/sp/design_extractor/PatternExtractor.cpp
+++ Team05/Code05/src/spa/src/sp/design_extractor/PatternExtractor.cpp
@@ -1,16 +1,4 @@
-// imported libraries
-#include <vector>
-
-
-// imported locally
 #include <sp/design_extractor/PatternExtractor.h>
-#include <sp/dataclasses/ast/AST.h>
-#include <sp/dataclasses/ast/BracketsASTNode.h>
-#include <sp/dataclasses/ast/ExpressionASTNode.h>
-#include <sp/dataclasses/ast/exceptions/ASTException.h>
-#include <sp/dataclasses/design_objects/Pattern.h>
-
-using namespace std;
 
 vector<Pattern> PatternExtractor::extract(shared_ptr<ASTNode> ast) {
 	ASTNodeType astType = ast->getType();
--- Team05/Code05/src/spa/src/sp/design_extractor/PatternExtractor.h
+++ Team05/Code05/src/spa/src/sp/design_extractor/PatternExtractor.h
@@ -1,9 +1,15 @@
 #pragma once
 #include <vector>
+#include <stack>
 #include <sp/dataclasses/design_objects/Pattern.h>
 #include <sp/dataclasses/ast/AST.h>
+#include <sp/dataclasses/ast/AssignASTNode.h>
+#include <sp/dataclasses/ast/BracketsASTNode.h>
+#include <sp/dataclasses/ast/ExpressionASTNode.h>
+#include <sp/dataclasses/ast/IfASTNode.h>
+#include <sp/dataclasses/ast/WhileASTNode.h>
+#include <sp/dataclasses/ast/exceptions/ASTException.h>
 #include <sp/design_extractor/Extractor.h>
-#include <stack>
 
 using namespace std;
 
