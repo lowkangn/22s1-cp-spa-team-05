384c5c835c89a91fa02ec43be19ec249ca7072b0
cpwill01 <williamming778@gmail.com>
2022-09-16 17:48:43 +0800

    Fix bug in initializing Relationships

--- Team05/Code05/src/unit_testing/src/qps/TestParentClauses.cpp
+++ Team05/Code05/src/unit_testing/src/qps/TestParentClauses.cpp
@@ -103,18 +103,18 @@ namespace {
 	Entity p1 = Entity::createPrintEntity(6);
 
 	//initialise parent relationships
-	Relationship parentW1A1 = Relationship(w1, a1, RelationshipType::PARENT);
-	Relationship parentW1I1 = Relationship(w1, i1, RelationshipType::PARENT);
-	Relationship parentI1R1 = Relationship(i1, r1, RelationshipType::PARENT);
-	Relationship parentI1R2 = Relationship(i1, r2, RelationshipType::PARENT);
+	Relationship parentW1A1 = Relationship::createParentRelationship(w1, a1);
+	Relationship parentW1I1 = Relationship::createParentRelationship(w1, i1);
+	Relationship parentI1R1 = Relationship::createParentRelationship(i1, r1);
+	Relationship parentI1R2 = Relationship::createParentRelationship(i1, r2);
 
 	//initialise parentStar relationships
-	Relationship parentStarW1A1 = Relationship(w1, a1, RelationshipType::PARENTT);
-	Relationship parentStarW1I1 = Relationship(w1, i1, RelationshipType::PARENTT);
-	Relationship parentStarI1R1 = Relationship(i1, r1, RelationshipType::PARENTT);
-	Relationship parentStarI1R2 = Relationship(i1, r2, RelationshipType::PARENTT);
-	Relationship parentStarW1R1 = Relationship(w1, r1, RelationshipType::PARENTT);
-	Relationship parentStarW1R2 = Relationship(w1, r2, RelationshipType::PARENTT);
+	Relationship parentStarW1A1 = Relationship::createParentTRelationship(w1, a1);
+	Relationship parentStarW1I1 = Relationship::createParentTRelationship(w1, i1);
+	Relationship parentStarI1R1 = Relationship::createParentTRelationship(i1, r1);
+	Relationship parentStarI1R2 = Relationship::createParentTRelationship(i1, r2);
+	Relationship parentStarW1R1 = Relationship::createParentTRelationship(w1, r1);
+	Relationship parentStarW1R2 = Relationship::createParentTRelationship(w1, r2);
 
 
 	// Initialise corresponding PQLEntities and PQLRleationships
@@ -254,62 +254,6 @@ TEST_CASE("ParentStarClause: test execute") {
 			REQUIRE(actualClauseResult == expectedClauseResult);
 
 	};
-
-	//initialise entities
-	Entity w1 = Entity::createWhileEntity(1);
-	Entity a1 = Entity::createAssignEntity(2);
-	Entity i1 = Entity::createIfEntity(3);
-	Entity r1 = Entity::createReadEntity(4);
-	Entity r2 = Entity::createReadEntity(5);
-	Entity p1 = Entity::createPrintEntity(6);
-
-	//initialise parent relationships
-	Relationship parentW1A1 = Relationship(w1, a1, RelationshipType::PARENT);
-	Relationship parentW1I1 = Relationship(w1, i1, RelationshipType::PARENT);
-	Relationship parentI1R1 = Relationship(i1, r1, RelationshipType::PARENT);
-	Relationship parentI1R2 = Relationship(i1, r2, RelationshipType::PARENT);
-
-	//initialise parentStar relationships
-	Relationship parentStarW1A1 = Relationship(w1, a1, RelationshipType::PARENTT);
-	Relationship parentStarW1I1 = Relationship(w1, i1, RelationshipType::PARENTT);
-	Relationship parentStarI1R1 = Relationship(i1, r1, RelationshipType::PARENTT);
-	Relationship parentStarI1R2 = Relationship(i1, r2, RelationshipType::PARENTT);
-	Relationship parentStarW1R1 = Relationship(w1, r1, RelationshipType::PARENTT);
-	Relationship parentStarW1R2 = Relationship(w1, r2, RelationshipType::PARENTT);
-
-
-	// Initialise corresponding PQLEntities and PQLRleationships
-	PQLEntity pqlW1 = PQLEntity::generateStatement(1);
-	PQLEntity pqlA1 = PQLEntity::generateStatement(2);
-	PQLEntity pqlI1 = PQLEntity::generateStatement(3);
-	PQLEntity pqlR1 = PQLEntity::generateStatement(4);
-	PQLEntity pqlR2 = PQLEntity::generateStatement(5);
-	PQLEntity pqlP1 = PQLEntity::generateStatement(6);
-
-	PQLRelationship pqlParentW1A1 = PQLRelationship(pqlW1, pqlA1);
-	PQLRelationship pqlParentW1I1 = PQLRelationship(pqlW1, pqlI1);
-	PQLRelationship pqlParentI1R1 = PQLRelationship(pqlI1, pqlR1);
-	PQLRelationship pqlParentI1R2 = PQLRelationship(pqlI1, pqlR2);
-
-	PQLRelationship pqlParentStarW1A1 = PQLRelationship(pqlW1, pqlA1);
-	PQLRelationship pqlParentStarW1I1 = PQLRelationship(pqlW1, pqlI1);
-	PQLRelationship pqlParentStarI1R1 = PQLRelationship(pqlI1, pqlR1);
-	PQLRelationship pqlParentStarI1R2 = PQLRelationship(pqlI1, pqlR2);
-	PQLRelationship pqlParentStarW1R1 = PQLRelationship(pqlW1, pqlR1);
-	PQLRelationship pqlParentStarW1R2 = PQLRelationship(pqlW1, pqlR2);
-
-	// Clause Arguments
-	ClauseArgument firstStmtArg = ClauseArgument("s1", ArgumentType::STMT);
-	ClauseArgument secondStmtArg = ClauseArgument("s2", ArgumentType::STMT);
-	ClauseArgument ifArg = ClauseArgument("if", ArgumentType::IF);
-	ClauseArgument whileArg = ClauseArgument("while", ArgumentType::WHILE);
-	ClauseArgument lineOneArg = ClauseArgument("1", ArgumentType::LINE_NUMBER);
-	ClauseArgument lineTwoArg = ClauseArgument("2", ArgumentType::LINE_NUMBER);
-	ClauseArgument lineFourArg = ClauseArgument("4", ArgumentType::LINE_NUMBER);
-	ClauseArgument lineSixArg = ClauseArgument("6", ArgumentType::LINE_NUMBER);
-	ClauseArgument wildcardArg = ClauseArgument("_", ArgumentType::WILDCARD);
-	ClauseArgument readArg = ClauseArgument("rr", ArgumentType::READ);
-	ClauseArgument printArg = ClauseArgument("pp", ArgumentType::PRINT);
 
 	// ------ PKB ------ 
 	shared_ptr<PKB> pkb = shared_ptr<PKB>(new PKB());
