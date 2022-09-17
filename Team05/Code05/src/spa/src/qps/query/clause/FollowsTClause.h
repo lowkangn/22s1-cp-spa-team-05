e02e696f548f2e6f29149cc6a6090aa95b8c9ea9
lowkangn <kangngee62@gmail.com>
2022-09-17 21:07:53 +0800

    Implement FollowsClause and FollowsTClause

--- Team05/Code05/src/spa/src/qps/query/clause/FollowsTClause.h
+++ Team05/Code05/src/spa/src/qps/query/clause/FollowsTClause.h
@@ -4,5 +4,9 @@
 
 class FollowsTClause : public RelationshipClause {
 public:
+    FollowsTClause(ClauseArgument lhs, ClauseArgument rhs)
+        : RelationshipClause(lhs, rhs) {};
+
     shared_ptr<RelationshipClauseResult> execute(shared_ptr<PKBQueryHandler> pkb) override;
+    bool equals(shared_ptr<RelationshipClause> other) override;
 };
