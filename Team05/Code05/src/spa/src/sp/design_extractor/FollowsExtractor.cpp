ee8de203be971d1ca8c50f29586509fdab1a2846
lowkangn <kangngee62@gmail.com>
2022-09-17 13:59:01 +0800

    Implement FollowsExtractor

--- Team05/Code05/src/spa/src/sp/design_extractor/FollowsExtractor.cpp
+++ Team05/Code05/src/spa/src/sp/design_extractor/FollowsExtractor.cpp
@@ -2,16 +2,54 @@
 
 #include <vector>
 
-#include <sp/design_extractor/FollowsExtractor.h>
 #include <sp/dataclasses/ast/IfASTNode.h>
+#include <sp/dataclasses/ast/ProcedureASTNode.h>
+#include <sp/dataclasses/ast/ProgramASTNode.h>
 #include <sp/dataclasses/ast/WhileASTNode.h>
+#include <sp/design_extractor/FollowsExtractor.h>
 
 using namespace std;
 
 vector<Relationship> FollowsExtractor::extract(shared_ptr<ASTNode> ast) {
-	return vector<Relationship>();
-}
+	vector<Relationship> extractedFollowsRelationships;
 
-vector<Relationship> FollowsExtractor::extractFromContainer(Entity leftHandSide, shared_ptr<ASTNode> containerASTNode) {
-	return vector<Relationship>();
+	if (ast->isIfNode()) {
+		shared_ptr<IfASTNode> ifNode = dynamic_pointer_cast<IfASTNode>(ast);
+
+		vector<Relationship> thenFollowsRelationships = this->handleStmtList(ifNode->getThenStatements());
+		vector<Relationship> elseFollowsRelationships = this->handleStmtList(ifNode->getElseStatements());
+
+		extractedFollowsRelationships.insert(extractedFollowsRelationships.end(), thenFollowsRelationships.begin(), thenFollowsRelationships.end());
+		extractedFollowsRelationships.insert(extractedFollowsRelationships.end(), elseFollowsRelationships.begin(), elseFollowsRelationships.end());
+	}
+
+	else if (ast->isWhileNode()) {
+		shared_ptr<WhileASTNode> whileNode = dynamic_pointer_cast<WhileASTNode>(ast);
+
+		vector<Relationship> whileFollowsRelationships = this->handleStmtList(whileNode->getStmtList());
+
+		extractedFollowsRelationships.insert(extractedFollowsRelationships.end(), whileFollowsRelationships.begin(), whileFollowsRelationships.end());
+	}
+
+	else if (ast->isProcedureNode() || ast->isProgramNode()) {
+		// For procedure node, recursively extract follows relationships from its statement list.
+		// For program node, recursively extract follows relationships from its procedures.
+		for (shared_ptr<ASTNode> child : ast->getChildren()) {
+			vector<Relationship> extractedRelations = this->extract(child);
+			extractedFollowsRelationships.insert(extractedFollowsRelationships.begin(), extractedRelations.begin(), extractedRelations.end());
+		}
+	}
+
+	else if (ast->isStmtLstNode()) {
+
+		vector<Relationship> followsRelationships = this->handleStmtList(ast);
+
+		extractedFollowsRelationships.insert(extractedFollowsRelationships.end(), followsRelationships.begin(), followsRelationships.end());
+	}
+
+	else {
+		throw ASTException("Unknown AST node was passed to FollowsExtractor!");
+	}
+
+	return extractedFollowsRelationships;
 }
--- Team05/Code05/src/spa/src/sp/design_extractor/FollowsExtractor.h
+++ Team05/Code05/src/spa/src/sp/design_extractor/FollowsExtractor.h
@@ -2,11 +2,45 @@
 #include <vector>
 #include <sp/dataclasses/design_objects/Relationship.h>
 #include <sp/dataclasses/ast/AST.h>
+#include <sp/dataclasses/ast/StatementListASTNode.h>
 #include <sp/design_extractor/Extractor.h>
 
 using namespace std;
 
 class FollowsExtractor : Extractor<Relationship> {
+
+private:
+	/*
+		This method is used to extract relationships from a Statement list node.
+	*/
+	vector<Relationship> handleStmtList(shared_ptr<ASTNode> ast) {
+		vector<Relationship> extractedFollowsRelationships;
+
+		shared_ptr<StatementListASTNode> stmtListNode = dynamic_pointer_cast<StatementListASTNode>(ast);
+		vector<shared_ptr<ASTNode>> children = stmtListNode->getChildren();
+
+		for (int i = 0; i < children.size(); i++) {
+
+			shared_ptr<ASTNode> child = children[i];
+
+			// Skip last child as there are no statements that follow it.
+			if (i < children.size() - 1) {
+				Entity followee = children[i]->extractEntity();
+				Entity follower = children[i + 1]->extractEntity();
+
+				Relationship toAdd = Relationship::createFollowsRelationship(followee, follower);
+				extractedFollowsRelationships.push_back(toAdd);
+			}
+
+			// If any statement is a if or while statement, extract the follows relationships within it.
+			if (child->isIfNode() || child->isWhileNode()) {
+				vector<Relationship> nestedFollowsRelationships = this->extract(child);
+				extractedFollowsRelationships.insert(extractedFollowsRelationships.end(), nestedFollowsRelationships.begin(), nestedFollowsRelationships.end());
+			}
+		}
+
+		return extractedFollowsRelationships;
+	}
 
 public:
 	/*
@@ -14,9 +48,4 @@ public:
 		overriden for each specific relationship type.
 	*/
 	virtual vector<Relationship> extract(shared_ptr<ASTNode> ast) = 0;
-
-	/*
-		Iterates through the children of a container node extracting the Follows relationships
-	*/
-	vector<Relationship> extractFromContainer(Entity leftHandSide, shared_ptr<ASTNode> containerASTNode);
 };
