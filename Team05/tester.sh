a26ec25077dfcfa07698896cedb84226d9673ca6
cpwill01 <williamming778@gmail.com>
2022-09-19 12:02:40 +0800

    Add more tests

--- Team05/tester.sh
+++ Team05/tester.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-readonly path_to_autotester=./Code05/out/build/x64-Debug/src/autotester/autotester.exe
+config=Debug
+while getopts r: flag
+do
+    case "${flag}" in
+        r) config=Release
+    esac
+done
+
+readonly path_to_autotester=./Code05/out/build/x64-${config}/src/autotester/autotester.exe
 readonly invalid_q_path=./Tests05/iteration1/invalidQuery/
 readonly invalid_s_path=./Tests05/iteration1/invalidSource/
 readonly no_constr_path=./Tests05/iteration1/noConstraintsQuery/
@@ -10,7 +18,7 @@ readonly output_folder=./Code05/testOutputs/
 
 
 echo "Starting invalid query test cases"
-for i in Declaration Follows Modifies Parent Select Uses
+for i in Declaration Follows Modifies Parent Select Uses Pattern
 do	
        ${path_to_autotester} ${invalid_q_path}invalid$i\_source.txt ${invalid_q_path}invalid$i\_queries.txt ${output_folder}invalidQueryOut$i.xml
 done
@@ -25,14 +33,14 @@ echo "Starting no constraints query test cases"
 ${path_to_autotester} ${no_constr_path}noConstraints_source.txt ${no_constr_path}noConstraints_queries.txt ${output_folder}outNoConstraints.xml
 
 echo "Starting one constraint query test cases"
-for i in follows modifies parent pattern Uses
+for i in follows modifies parent pattern uses
 do
 	${path_to_autotester} ${one_constr_path}${i}Only_source.txt ${one_constr_path}${i}Only_queries.txt ${output_folder}${i}onlyOut.xml
 done
 
-#TODO: ADD THE REST (follows modifies uses)
+#TODO: Add modifies to the list below when it's ready
 echo "Starting two constraint query test cases"
-for i in parent uses
+for i in parent uses follows modifies
 do
 ${path_to_autotester} ${two_constr_path}${i}AndPattern_source.txt ${two_constr_path}${i}AndPattern_queries.txt ${output_folder}${i}AndPatternOut.xml
 done
