879b2371a6bbb7b66f83f60921c9293d571a3d26
cpwill01 <williamming778@gmail.com>
2022-09-30 18:33:02 +0800

    Remove stray comment in Query.cpp

--- Team05/Code05/src/spa/src/qps/query/Query.cpp
+++ Team05/Code05/src/spa/src/qps/query/Query.cpp
@@ -20,8 +20,6 @@ list<shared_ptr<RelationshipClauseResult>> Query::executeSuchThatAndPattern(shar
 bool operator==(Query first, Query second) {
 	bool isSelectClauseEqual = (*(first.selectClause.get())).equals(second.selectClause.get());
 
-	//TODO: Refactor similar logic into private generic helper method e.g. checkListsEqual<T>( list<shared_ptr<T>> a, list<shared_ptr<T>> b)
-
 	if (!isSelectClauseEqual) {
 		// different select clauses
 		return false;
@@ -32,40 +30,6 @@ bool operator==(Query first, Query second) {
 		// different pattern clauses
 		return false;
 	}
-	return true;
-	/*
-	else if (first.suchThatClauses.size() != second.suchThatClauses.size()) {
-		// different number of such that clauses after Select
-		return false;
-	}
-	else if (first.patternClauses.size() != second.patternClauses.size()) {
-		// different number of pattern clauses after Select
-		return false;
-	}
-	
-	// check remaining clauses sequentially
-	list<shared_ptr<RelationshipClause>>::iterator firstIter = first.suchThatClauses.begin();
-	list<shared_ptr<RelationshipClause>>::iterator secondIter = second.suchThatClauses.begin();
-	while (firstIter != first.suchThatClauses.end()) {
-		isClauseEqual = (*firstIter)->equals(*secondIter);
-		if (!isClauseEqual) {
-			return false;
-		}
-		firstIter++;
-		secondIter++;
-	}
 
-	list<shared_ptr<PatternClause>>::iterator firstPatternIter = first.patternClauses.begin();
-	list<shared_ptr<PatternClause>>::iterator secondPatternIter = second.patternClauses.begin();
-	while (firstPatternIter != first.patternClauses.end()) {
-		isClauseEqual = (*firstPatternIter)->equals(*secondPatternIter);
-		if (!isClauseEqual) {
-			return false;
-		}
-		firstPatternIter++;
-		secondPatternIter++;
-	}
-	
-	return true;*/
-	
+	return true;
 }
