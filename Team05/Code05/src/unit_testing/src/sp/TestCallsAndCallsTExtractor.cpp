539fa6939b910faadde4a81acfb48be22dc445c3
lowkangn <kangngee62@gmail.com>
2022-10-03 20:58:12 +0800

    Add test cases for invalid programs

--- Team05/Code05/src/unit_testing/src/sp/TestCallsAndCallsTExtractor.cpp
+++ Team05/Code05/src/unit_testing/src/sp/TestCallsAndCallsTExtractor.cpp
@@ -4,7 +4,7 @@
 #include <sp/dataclasses/design_objects/Relationship.h>
 #include <sp/dataclasses/tokens/Token.h>
 #include <sp/dataclasses/ast/AST.h>
-#include <vector>
+#include <vector>	
 #include <memory>
 #include <sp/dataclasses/ast/CallASTNode.h>
 #include <sp/dataclasses/ast/ProcedureASTNode.h>
@@ -46,14 +46,17 @@ TEST_CASE("CallsAndCallsTExtractor: test extract") {
 			procedure beta {}
 		*/
 		
+		// procedure main
 		shared_ptr<ASTNode> mainProcedureNode = ProcedureASTNode::createProcedureNode(mainToken);
 		shared_ptr<ASTNode> mainStmtListNode = StatementListASTNode::createStatementListNode();
 		mainProcedureNode->addChild(mainStmtListNode);
 
+		// procedure alpha
 		shared_ptr<ASTNode> alphaProcedureNode = ProcedureASTNode::createProcedureNode(alphaToken);
 		shared_ptr<ASTNode> alphaStmtListNode = StatementListASTNode::createStatementListNode();
 		alphaProcedureNode->addChild(alphaStmtListNode);
 
+		// procedure beta
 		shared_ptr<ASTNode> betaProcedureNode = ProcedureASTNode::createProcedureNode(betaToken);
 		shared_ptr<ASTNode> betaStmtListNode = StatementListASTNode::createStatementListNode();
 		betaProcedureNode->addChild(betaStmtListNode);
@@ -128,5 +131,61 @@ TEST_CASE("CallsAndCallsTExtractor: test extract") {
 		vector<Relationship> expectedCallsRelationships{ callsMainAlpha, callsMainBeta, callsAlphaBeta };
 
 		test(programNode, expectedCallsRelationships);
+	}	
+}
+
+TEST_CASE("CallsAndCallsTExtractor: test extractWithSemanticError") {
+	auto testExtractWithError = [](shared_ptr<ASTNode> astNode, string expectedErrorMessage) {
+			// given
+			CallsAndCallsTExtractor extractor = CallsAndCallsTExtractor();
+
+			// then
+			REQUIRE_THROWS_MATCHES(extractor.extract(astNode), ASTException, Catch::Message(expectedErrorMessage));
+	};
+
+	// Variables are omitted for brevity. CallsAndCallsTExtractor does not interact with variables.
+	Token mainToken = Token::createNameOrKeywordToken("main");
+	Token alphaToken = Token::createNameOrKeywordToken("alpha");
+	Token betaToken = Token::createNameOrKeywordToken("beta");
+	Token callToken = Token::createCallToken();
+
+	SECTION("Procedure calling itself") {
+
+		/*
+			procedure main {
+				call main;
+			}
+		*/
+
+		shared_ptr<ASTNode> mainProcedureNode = ProcedureASTNode::createProcedureNode(mainToken);
+		shared_ptr<ASTNode> mainStmtListNode = StatementListASTNode::createStatementListNode();
+		mainProcedureNode->addChild(mainStmtListNode);
+
+		shared_ptr<ASTNode> callMainNode = CallASTNode::createCallNode();
+		callMainNode->addChild(mainProcedureNode);
+		mainStmtListNode->addChild(callMainNode);
+
+		testExtractWithError(mainProcedureNode, "A procedure is calling itself!");
 	}
+
+	SECTION("Calling non-existent procedure") {
+		/*
+			procedure main {
+				call alpha;
+			}
+		*/
+
+		shared_ptr<ASTNode> mainProcedureNode = ProcedureASTNode::createProcedureNode(mainToken);
+		shared_ptr<ASTNode> mainStmtListNode = StatementListASTNode::createStatementListNode();
+		mainProcedureNode->addChild(mainStmtListNode);
+
+		shared_ptr<ASTNode> alphaProcedureNode = ProcedureASTNode::createProcedureNode(alphaToken);
+
+		shared_ptr<ASTNode> callMainNode = CallASTNode::createCallNode();
+		callMainNode->addChild(alphaProcedureNode);
+		mainStmtListNode->addChild(callMainNode);
+
+		testExtractWithError(mainProcedureNode, "Trying to call a non-existent procedure alpha");
+	}
+
 }
