14816277048e4369587a86d80acc41df088acee0
lowkangn <kangngee62@gmail.com>
2022-10-04 14:41:31 +0800

    Add CallSimpleSyntaxRule

---
+++ Team05/Code05/src/unit_testing/src/sp/TestCallSimpleSyntaxRule.cpp
@@ -0,0 +1,13 @@
+#include "catch.hpp"
+
+#include <sp/dataclasses/ast/AST.h>
+#include <sp/dataclasses/ast/ProcedureASTNode.h>
+#include <sp/dataclasses/ast/StatementListASTNode.h>\
+#include <sp/dataclasses/ast/ProgramASTNode.h>
+
+#include <sp/dataclasses/tokens/Token.h>
+#include <list>
+#include <memory>
+
+#include <sp/parser/rules/CallSimpleSyntaxRule.h>
+
