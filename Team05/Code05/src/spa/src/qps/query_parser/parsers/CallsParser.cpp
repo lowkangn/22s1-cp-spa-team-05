1b6e2fd33792f4ed2a5f1e972d2ad3a54be55a2b
lowkangn <kangngee62@gmail.com>
2022-10-09 20:19:03 +0800

    Implement calls parser

---
+++ Team05/Code05/src/spa/src/qps/query_parser/parsers/CallsParser.cpp
@@ -0,0 +1,52 @@
+#include <qps/query_parser/parsers/CallsParser.h>
+
+bool CallsParser::isCorrectClauseType(PQLToken clauseTypeToken) {
+	if (tokens.size() == 1) {
+		throw PQLSyntaxError("Query ended after 'Calls'");
+	}
+
+	PQLToken secondToken = *std::next(this->tokens.begin());
+	if (secondToken.isStarModifier()) {
+		this->isStar = true;
+		this->tokens.pop_front();
+	}
+
+	return clauseTypeToken.isCalls();
+}
+
+void CallsParser::checkArguments(list<ClauseArgument>& args) {
+	// Calls clause should have exactly 2 arguments
+	assert(args.size() == 2);
+
+
+	// first arg cannot be statement, variable or constant
+	ClauseArgument arg = args.front();
+	if (arg.isStmtSynonym(), arg.isVariableSynonym() || arg.isConstantSynonym()) {
+		this->semanticErrorMessage = "First arg for Uses cannot be statement, variable or constant";
+	}
+
+	arg = args.back();
+
+	// semantically, both args must be either wildcards or procedures
+	for (ClauseArgument arg : args) {
+		if (arg.isLineNumber()) {
+			throw PQLSyntaxError("Each arg for Calls/Calls* cannot be a number");
+		}
+
+		if (!arg.isWildcard() && !arg.isProcedureSynonym() && !arg.isStringLiteral()) {
+			this->semanticErrorMessage = "Each arg for Calls/Calls* must be must be a wildcard, a \"NAME\" or a declared procedure synonym";
+		}
+	}
+}
+
+shared_ptr<RelationshipClause> CallsParser::createClause(PQLToken clauseTypeToken, list<ClauseArgument>& args) {
+	//Calls clause should have exactly 2 arguments
+	assert(args.size() == 2);
+
+	if (this->isStar) {
+		return shared_ptr<RelationshipClause>(new CallsTClause(args.front(), args.back()));
+	}
+	else {
+		return shared_ptr<RelationshipClause>(new CallsClause(args.front(), args.back()));
+	}
+}
