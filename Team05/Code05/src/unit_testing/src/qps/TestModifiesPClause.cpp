04e67572e84a949bcef7f8fd7404edee9242a9c2
lowkangn <kangngee62@gmail.com>
2022-10-15 23:35:48 +0800

    Add integration test with PKB for ModifiesPClause

--- Team05/Code05/src/unit_testing/src/qps/TestModifiesPClause.cpp
+++ Team05/Code05/src/unit_testing/src/qps/TestModifiesPClause.cpp
@@ -70,14 +70,178 @@ TEST_CASE("ModifiesPClause: test equals") {
 	}
 
 	SECTION("Not even a ModifiesPClause") {
-		shared_ptr<RelationshipClause> modifiesSClause(new ModifiesSClause(stmtArg, firstVarArg));
-		testEquals(modifiesSClause, false);
-
-		// TODO: Add more when other Clause classes are implemented
+		shared_ptr<RelationshipClause> modifiesPClause(new ModifiesSClause(stmtArg, firstVarArg));
+		testEquals(modifiesPClause, false);
 	}
 }
 
 // =============== INTEGRATION TESTS ====================
+namespace {
+	/* Corresponds to the following SIMPLE source (with line numbers)
+		procedure main {
+			1. read y;
+			2. x = y;
+			3. while (x > 0) {
+			4.	   print z;
+			5.     x = x - 1;
+			   }
+		}
 
-// TODO: Requires PKB interaction implementation
-TEST_CASE("ModifiesPClause: test execute") {}
+		procedure test {
+			6. print b;
+			7. x = y;
+		}
+
+		procedure noUses {
+			8. print y;
+		}
+	*/
+
+	// Initialise tokens
+	Token mainToken = Token::createNameOrKeywordToken("main");
+	Token testToken = Token::createNameOrKeywordToken("test");
+	Token noUsesToken = Token::createNameOrKeywordToken("noUses");
+	Token yToken = Token::createNameOrKeywordToken("y");
+	Token xToken = Token::createNameOrKeywordToken("x");
+
+	// Initialise entities
+	Entity mainEntity = Entity::createProcedureEntity(mainToken);
+	Entity testEntity = Entity::createProcedureEntity(testToken);
+
+	Entity y1Entity = Entity::createVariableEntity(1, yToken);
+	Entity x2Entity = Entity::createVariableEntity(2, xToken);
+	Entity x5LhsEntity = Entity::createVariableEntity(5, xToken);
+	Entity x7Entity = Entity::createVariableEntity(7, xToken);
+
+	// Initialise modifies relationships
+	Relationship modifiesMainY1 = Relationship::createModifiesRelationship(mainEntity, y1Entity);
+	Relationship modifiesMainX2 = Relationship::createModifiesRelationship(mainEntity, x2Entity);
+	Relationship modifiesMainX5 = Relationship::createModifiesRelationship(mainEntity, x5LhsEntity);
+
+	Relationship modifiesTestX7 = Relationship::createModifiesRelationship(testEntity, x7Entity);
+
+	// Initialise corresponding PQLEntities and PQLRelationships
+	PQLEntity pqlMain = PQLEntity::generateProcedure("main");
+	PQLEntity pqlTest = PQLEntity::generateProcedure("test");
+
+	PQLEntity pqlVarX = PQLEntity::generateVariable("x");
+	PQLEntity pqlVarY = PQLEntity::generateVariable("y");
+
+	PQLRelationship pqlModifiesMainY = PQLRelationship(pqlMain, pqlVarY);
+	PQLRelationship pqlModifiesMainX = PQLRelationship(pqlMain, pqlVarX);
+
+	PQLRelationship pqlModifiesTestX = PQLRelationship(pqlTest, pqlVarX);
+
+	// Clause Arguments
+	ClauseArgument procedureArg = ClauseArgument::createProcedureArg("p");
+	ClauseArgument variableArg = ClauseArgument::createVariableArg("v");
+	ClauseArgument mainLiteralArg = ClauseArgument::createStringLiteralArg("main");
+	ClauseArgument testLiteralArg = ClauseArgument::createStringLiteralArg("test");
+	ClauseArgument noUsesLiteralArg = ClauseArgument::createStringLiteralArg("noUses");
+	ClauseArgument xLiteralArg = ClauseArgument::createStringLiteralArg("x");
+	ClauseArgument yLiteralArg = ClauseArgument::createStringLiteralArg("y");
+	ClauseArgument zLiteralArg = ClauseArgument::createStringLiteralArg("z");
+};
+
+TEST_CASE("ModifiesPClause: test execute") {
+	auto testExecute = [](ModifiesPClause modifiesPClause,
+		RelationshipClauseResult expectedClauseResult,
+		shared_ptr<PKB> pkb) {
+			// given
+			shared_ptr<PKBQueryHandler> pkbInterface = shared_ptr<PKBQueryHandler>(pkb);
+
+			// when
+			shared_ptr<RelationshipClauseResult> resPtr = modifiesPClause.execute(pkbInterface);
+			RelationshipClauseResult actualClauseResult = *resPtr;
+
+			// then
+			REQUIRE(actualClauseResult == expectedClauseResult);
+
+	};
+
+	// ------ PKB ------ 
+	shared_ptr<PKB> pkb = shared_ptr<PKB>(new PKB());
+
+	vector<Entity> entities{ mainEntity, testEntity, y1Entity, x2Entity, x5LhsEntity, x7Entity };
+
+	vector<Relationship> relationships{ modifiesMainY1, modifiesMainX2, modifiesMainX5, modifiesTestX7 };
+
+	pkb->addEntities(entities);
+	pkb->addRelationships(relationships);
+
+
+	// ------ QPS ------ 
+	ModifiesPClause clause = ModifiesPClause(procedureArg, variableArg);
+
+	vector<PQLRelationship> expectedRetrievedFromPkb = { pqlModifiesMainY, pqlModifiesMainX, pqlModifiesTestX };
+
+	RelationshipClauseResult expectedClauseResult = RelationshipClauseResult(procedureArg, variableArg, expectedRetrievedFromPkb);
+
+	SECTION("One procedure synonym and one variable synonym - all statement modifies") {
+		testExecute(clause, expectedClauseResult, pkb);
+	}
+
+	SECTION("Procedure string literal - non empty results") {
+		clause = ModifiesPClause(mainLiteralArg, variableArg);
+		expectedRetrievedFromPkb = { pqlModifiesMainY, pqlModifiesMainX };
+		expectedClauseResult = RelationshipClauseResult(mainLiteralArg, variableArg, expectedRetrievedFromPkb);
+		testExecute(clause, expectedClauseResult, pkb);
+
+		clause = ModifiesPClause(testLiteralArg, variableArg);
+		expectedRetrievedFromPkb = { pqlModifiesTestX };
+		expectedClauseResult = RelationshipClauseResult(testLiteralArg, variableArg, expectedRetrievedFromPkb);
+		testExecute(clause, expectedClauseResult, pkb);
+	}
+
+	SECTION("Procedure string literal - empty results") {
+		expectedRetrievedFromPkb = {};
+
+		clause = ModifiesPClause(noUsesLiteralArg, variableArg);
+		expectedClauseResult = RelationshipClauseResult(noUsesLiteralArg, variableArg, expectedRetrievedFromPkb);
+		testExecute(clause, expectedClauseResult, pkb);
+	}
+
+	SECTION("Variable string literal - non empty results") {
+		clause = ModifiesPClause(procedureArg, yLiteralArg);
+		expectedRetrievedFromPkb = { pqlModifiesMainY };
+		expectedClauseResult = RelationshipClauseResult(procedureArg, yLiteralArg, expectedRetrievedFromPkb);
+		testExecute(clause, expectedClauseResult, pkb);
+
+		clause = ModifiesPClause(procedureArg, xLiteralArg);
+		expectedRetrievedFromPkb = { pqlModifiesMainX, pqlModifiesTestX };
+		expectedClauseResult = RelationshipClauseResult(procedureArg, xLiteralArg, expectedRetrievedFromPkb);
+		testExecute(clause, expectedClauseResult, pkb);
+	}
+
+	SECTION("Variable string literal - empty results") {
+		expectedRetrievedFromPkb = {};
+
+		clause = ModifiesPClause(procedureArg, zLiteralArg);
+		expectedClauseResult = RelationshipClauseResult(procedureArg, zLiteralArg, expectedRetrievedFromPkb);
+		testExecute(clause, expectedClauseResult, pkb);
+	}
+
+	SECTION("Procedure and variable string literals - non empty results") {
+		clause = ModifiesPClause(mainLiteralArg, xLiteralArg);
+		expectedRetrievedFromPkb = { pqlModifiesMainX };
+		expectedClauseResult = RelationshipClauseResult(mainLiteralArg, xLiteralArg, expectedRetrievedFromPkb);
+		testExecute(clause, expectedClauseResult, pkb);
+
+		clause = ModifiesPClause(testLiteralArg, xLiteralArg);
+		expectedRetrievedFromPkb = { pqlModifiesTestX };
+		expectedClauseResult = RelationshipClauseResult(testLiteralArg, xLiteralArg, expectedRetrievedFromPkb);
+		testExecute(clause, expectedClauseResult, pkb);
+	}
+
+	SECTION("Procedure and variable string literals - empty results") {
+		expectedRetrievedFromPkb = {};
+
+		clause = ModifiesPClause(mainLiteralArg, zLiteralArg);
+		expectedClauseResult = RelationshipClauseResult(mainLiteralArg, zLiteralArg, expectedRetrievedFromPkb);
+		testExecute(clause, expectedClauseResult, pkb);
+
+		clause = ModifiesPClause(testLiteralArg, yLiteralArg);
+		expectedClauseResult = RelationshipClauseResult(testLiteralArg, yLiteralArg, expectedRetrievedFromPkb);
+		testExecute(clause, expectedClauseResult, pkb);
+	}
+}
