94ce7548ee30c93b96742cff34ab17629b30e660
tanyjnaaman <tannaaman@hotmail.com>
2022-10-16 11:24:32 +0800

    Add tests

--- Team05/tester.sh
+++ Team05/tester.sh
@@ -1,5 +1,6 @@
  #!/bin/bash
 
+ # config
 config=Debug
 while getopts 'r' flag
 do
@@ -8,19 +9,33 @@ do
     esac
 done
 
-readonly iteration1=./Tests05/iteration1/
-readonly iteration2=./Tests05/iteration2/
+# ==================== test config ====================
+# autotester path
 readonly path_to_autotester=./Code05/out/build/x64-${config}/src/autotester/autotester.exe
+
+# 0. sample
+readonly sample_path=./Tests05/
+
+# 1. iteration 1
+readonly iteration1=./Tests05/iteration1/
+readonly iteration1_output_folder=./Code05/testOutputs/iteration1/
+
+# 2. iteration 1 bug fix
+readonly milestoneBugs_path=./Tests05/iteration1/conditionsInWhile/
+
+# 2. iteration 2
+readonly iteration2=./Tests05/iteration2/
+readonly iteration2_output_folder=./Code05/testOutputs/iteration2/
+
 readonly invalid_q_path=invalidQuery/
 readonly invalid_s_path=invalidSource/
 readonly no_constr_path=noConstraintsQuery/
 readonly one_constr_path=oneConstraintQuery/
 readonly two_constr_path=twoConstraintsQuery/
-readonly milestoneBugs_path=./Tests05/iteration1/conditionsInWhile/
-readonly iteration1_output_folder=./Code05/testOutputs/iteration1/
-readonly iteration2_output_folder=./Code05/testOutputs/iteration2/
-readonly sample_path=./Tests05/
+readonly boolean_return_values_path=booleanReturnValues/
 
+
+# ==================== run tests ====================
 if [ ! -d $iteration1_output_folder ]; then
 	echo "Making output folder: "${iteration1_output_folder}
 	mkdir -p $iteration1_output_folder
@@ -32,6 +47,7 @@ if [ ! -d $iteration2_output_folder ]; then
 fi
 
 echo "Using autotester at: "${path_to_autotester}
+
 
 echo "Starting invalid query test cases"
 for i in Declaration Follows Modifies Parent Select Uses Pattern
@@ -63,7 +79,6 @@ done
 
 echo "Starting no constraints query test cases"
 ${path_to_autotester} ${iteration1}${no_constr_path}noConstraints_source.txt ${iteration1}${no_constr_path}noConstraints_queries.txt ${iteration1_output_folder}outNoConstraints.xml
-
 ${path_to_autotester} ${iteration2}${no_constr_path}noConstraints_source.txt ${iteration2}${no_constr_path}noConstraints_queries.txt ${iteration2_output_folder}outNoConstraints.xml
 
 echo "Starting one constraint query test cases"
@@ -82,6 +97,18 @@ for i in parent uses follows modifies
 do
     ${path_to_autotester} ${iteration1}${two_constr_path}${i}AndPattern_source.txt ${iteration1}${two_constr_path}${i}AndPattern_queries.txt ${iteration1_output_folder}${i}AndPatternOut.xml
 done
+
+
+echo "starting boolean return value test cases"
+# no constraints
+${path_to_autotester} ${iteration2}${boolean_return_values_path}noConstraints_source.txt ${iteration2}${boolean_return_values_path}noConstraints_queries.txt ${iteration2_output_folder}booleanReturnValuesNoConstraintsOut.xml
+
+# all false
+${path_to_autotester} ${iteration2}${boolean_return_values_path}noneSatisfyingReturnFalse_source.txt ${iteration2}${boolean_return_values_path}noneSatisfyingReturnFalse_queries.txt ${iteration2_output_folder}booleanReturnValuesNoneSatisfyingOut.xml
+
+
+# all true
+
 
 # For random testing, modify Sample_queries.txt and Sample_source.txt
 echo "starting local sample test cases"
