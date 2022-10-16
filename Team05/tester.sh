a098a63a6a19a57c2f39a2e0490eedf95c23a300
tanyjnaaman <tannaaman@hotmail.com>
2022-10-16 13:28:37 +0800

    Add basic program test cases

--- Team05/tester.sh
+++ Team05/tester.sh
@@ -49,54 +49,6 @@ fi
 echo "Using autotester at: "${path_to_autotester}
 
 
-echo "Starting invalid query test cases"
-for i in Declaration Follows Modifies Parent Select Uses Pattern
-do	
-    ${path_to_autotester} ${iteration1}${invalid_q_path}invalid$i\_source.txt ${iteration1}${invalid_q_path}invalid$i\_queries.txt ${iteration1_output_folder}invalidQueryOut$i.xml
-done
-
-for i in Calls
-do	
-    ${path_to_autotester} ${iteration2}${invalid_q_path}invalid$i\_source.txt ${iteration2}${invalid_q_path}invalid$i\_queries.txt ${iteration2_output_folder}invalidQueryOut$i.xml
-done
-
-echo "Starting invalid source test cases"
-for i in {1..19}
-do	
-        ${path_to_autotester} ${iteration1}${invalid_s_path}invalidSource$i\_source.txt ${iteration1}${invalid_s_path}invalidSource$i\_queries.txt ${iteration1_output_folder}invalidSourceOut$i.xml
-done
-
-for i in {1..4}
-do	
-        ${path_to_autotester} ${iteration2}${invalid_s_path}invalidSource$i\_source.txt ${iteration2}${invalid_s_path}invalidSource$i\_queries.txt ${iteration2_output_folder}invalidSourceOut$i.xml
-done
-
-echo "Starting milestone test cases"
-for i in {1..3}
-do	
-    ${path_to_autotester} ${milestoneBugs_path}whileStatementTest$i\_source.txt ${milestoneBugs_path}whileStatementTest$i\_queries.txt ${iteration1_output_folder}whileStatementTest$i.xml
-done
-
-echo "Starting no constraints query test cases"
-${path_to_autotester} ${iteration1}${no_constr_path}noConstraints_source.txt ${iteration1}${no_constr_path}noConstraints_queries.txt ${iteration1_output_folder}outNoConstraints.xml
-${path_to_autotester} ${iteration2}${no_constr_path}noConstraints_source.txt ${iteration2}${no_constr_path}noConstraints_queries.txt ${iteration2_output_folder}outNoConstraints.xml
-
-echo "Starting one constraint query test cases"
-for i in follows modifies parent pattern uses
-do
-    ${path_to_autotester} ${iteration1}${one_constr_path}${i}Only_source.txt ${iteration1}${one_constr_path}${i}Only_queries.txt ${iteration1_output_folder}${i}onlyOut.xml
-done
-
-for i in calls
-do
-    ${path_to_autotester} ${iteration2}${one_constr_path}${i}Only_source.txt ${iteration2}${one_constr_path}${i}Only_queries.txt ${iteration2_output_folder}${i}onlyOut.xml
-done
-
-echo "Starting two constraint query test cases"
-for i in parent uses follows modifies
-do
-    ${path_to_autotester} ${iteration1}${two_constr_path}${i}AndPattern_source.txt ${iteration1}${two_constr_path}${i}AndPattern_queries.txt ${iteration1_output_folder}${i}AndPatternOut.xml
-done
 
 
 echo "starting boolean return value test cases"
@@ -106,10 +58,9 @@ ${path_to_autotester} ${iteration2}${boolean_return_values_path}noConstraints_so
 # all false
 ${path_to_autotester} ${iteration2}${boolean_return_values_path}noneSatisfyingReturnFalse_source.txt ${iteration2}${boolean_return_values_path}noneSatisfyingReturnFalse_queries.txt ${iteration2_output_folder}booleanReturnValuesNoneSatisfyingOut.xml
 
+# all true
+${path_to_autotester} ${iteration2}${boolean_return_values_path}someSatistyingReturnTrue_source.txt ${iteration2}${boolean_return_values_path}someSatistyingReturnTrue_queries.txt ${iteration2_output_folder}booleanReturnValuesSomeSatisfyingOut.xml
+
 
 # all true
 
-
-# For random testing, modify Sample_queries.txt and Sample_source.txt
-echo "starting local sample test cases"
-${path_to_autotester} ${sample_path}Sample_source.txt ${sample_path}Sample_queries.txt ${iteration1_output_folder}SampleOut.xml
