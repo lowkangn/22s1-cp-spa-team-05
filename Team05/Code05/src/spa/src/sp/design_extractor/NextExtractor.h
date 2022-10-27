87974bd17a49ff920e64a793e524ad4e629d9806
Aryan Sarswat <65006917+AryanSarswat@users.noreply.github.com>
2022-10-27 21:21:07 +0800

    Create Base class for CFGExtractor

--- Team05/Code05/src/spa/src/sp/design_extractor/NextExtractor.h
+++ Team05/Code05/src/spa/src/sp/design_extractor/NextExtractor.h
@@ -2,17 +2,17 @@
 #include <vector>
 #include <sp/dataclasses/design_objects/Relationship.h>
 #include <sp/dataclasses/cfg/CFGNode.h>
-#include <sp/design_extractor/Extractor.h>
+#include <sp/design_extractor/CFGExtractor.h>
 
 #include <memory>
 
 using namespace std;
 
-class NextExtractor {
+class NextExtractor : public CFGExtractor<Relationship> {
 
 public:
 	/*
 		This method is used to extract relationships from a provided control flow graph.
 	*/
-	virtual vector<Relationship> extract(shared_ptr<CFGNode> cfg);
+	vector<Relationship> extract(shared_ptr<CFGNode> cfg) override;
 };
